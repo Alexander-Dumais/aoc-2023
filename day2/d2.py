a79d983b74297e44132ffcc3b178069a714295aa
Alexander-Dumais <alexander.dumais@outlook.com>
2023-12-05 01:48:30 -0600

    Starting Day3

--- day2/d2.py
+++ day2/d2.py
@@ -1,11 +1,12 @@
 from typing import List, Dict
 
+
 def read_games(filename):
     lines = []
     with open(filename) as f:
         for line in f:
             lines.append(line[5:].rstrip("\n"))
-    
+
     return lines
 
 
@@ -19,7 +20,7 @@ def decode_games(lines):
         game_num, games = game.split(": ")
         hands = games.split("; ")
         game_map[int(game_num)] = hands
-    
+
     return game_map
 
 
@@ -27,7 +28,7 @@ def count_colours(blocks):
     counts = {"red": 0, "green": 0, "blue": 0}
 
     for block in blocks:
-        count, colour = block.split( )
+        count, colour = block.split()
         counts[colour] = int(count)
 
     return counts
@@ -49,7 +50,7 @@ def all_valid_games(game_map: Dict[int, List[str]], config: Dict[str, int]):
         if not invalid:
             valid_games.append(game_id)
         # print()
-    
+
     return valid_games
 
 
@@ -61,10 +62,10 @@ def max_of_each_game(game_map: Dict[int, List[str]]):
         for hand in hands:
             hand_colours = count_colours(hand.split(","))
             for colour, count in hand_colours.items():
-                max_colours[colour] = max(count, max_colours[colour]) # Update new bhighest count for that colour
-        
+                max_colours[colour] = max(count, max_colours[colour])  # Update new bhighest count for that colour
+
         max_cubes_sums.append(max_colours["red"] * max_colours["green"] * max_colours["blue"])
-    
+
     return max_cubes_sums
 
 
@@ -77,5 +78,3 @@ if __name__ == "__main__":
 
     max_cubes = max_of_each_game(game_map)
     print(f"Sum of product of max cube counts to make for a valid config: {sum(max_cubes)}")
-
-    
