4cf490e7612ff5aa5269c124a7768113cc8a279c
Alexander-Dumais <alexander.dumais@outlook.com>
2023-12-27 14:19:48 -0600

    solution created, but slow.

--- day5/d5.py
+++ day5/d5.py
@@ -29,13 +29,45 @@ def parse_almanac(filename, keys_locations):
     return almanac
 
 
+def source_to_destination(s_to_d, target):
+    """
+    If the source range contains the target, return the location in the destination range
+     where the target was found. If no target is found, the destination is the target.
+    """
+    destination = target
+    for sd_map in s_to_d:
+        source_range = range(sd_map[1], sd_map[1] + sd_map[2])
+        offset = 0
+        for value in source_range:
+            if target == value:
+                destination = sd_map[0] + offset
+            offset += 1
+
+    return destination
 
 
+keys = ("sample_input.txt", [3, 7, 12, 18, 22, 27, 31])
+# keys = ("input", [3, 37, 57, 107, 142, 180, 211])
+seed_almanac = parse_almanac(keys[0], keys[1])
 
-sample_keys = [3, 7, 12, 18, 22, 27, 31]
-real_keys = [3, 37, 57, 107, 142, 180, 211]
-seed_almanac = parse_almanac('sample_input.txt', sample_keys)
-# seed_almanac = parse_almanac("input.txt", real_keys)
+# new mapping for simplicity
+s_to_d_mapping = {
+    "seeds": seed_almanac["seeds"],
+    "seed-to-soil": [],
+    "soil-to-fertilizer": [],
+    "fertilizer-to-water": [],
+    "water-to-light": [],
+    "light-to-temperature": [],
+    "temperature-to-humidity": [],
+    "humidity-to-location": []}
 
-for k,v in seed_almanac.items():
-    print(k, v)
+# Process of calculating source to destination for each starting seed
+key = 1
+keys = ["seeds", "seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light", "light-to-temperature", "temperature-to-humidity", "humidity-to-location"]
+while key < len(keys):
+    print(keys[key])
+    s_to_d_mapping[keys[key]] = [source_to_destination(seed_almanac[keys[key]], i) for i in s_to_d_mapping[keys[key - 1]]]
+    key += 1
+
+print(s_to_d_mapping["humidity-to-location"])
+print("lowest location:", min(s_to_d_mapping["humidity-to-location"]))
